f705ea24accc10ed1872508420a6acc2be4e0559
yahellio <yayakn1h@gmail.com>
2025-03-21 11:44:27 +0300

    Add: cypher func

--- Lab2/Code/react-app/src/Cypher.js
+++ Lab2/Code/react-app/src/Cypher.js
@@ -1,11 +1,33 @@
-export function Encrypt(val, key){
-
-
-    return val + "1" + key;
-}
-
-export function Decrypt(val, key){
-
+//x^28 + x^3 + 1
+ 
+const x28 = 27;
+const x3 = 2;
+ 
+function createArr(key){
+    const charArray = key.split('');
     
-    return val + "1" + key;
+    const binaryArray = charArray.map(char => parseInt(char, 2));
+    
+    return binaryArray;
 }
+ 
+export function Ncrypt(val, key){
+    let res = [];
+    let reg = createArr(key);
+    for(let i = 0; i < val.length; i++){
+        let xorResult = reg[x3] ^ reg[x28];
+        let binaryXOR = xorResult.toString(2);
+        res.unshift(binaryXOR);
+
+        reg.push(binaryXOR);
+        reg.shift();
+    }
+
+    const valBits = val.split('').map(bit => parseInt(bit, 2));
+
+    const xorWithVal = valBits.map((bit, index) => bit ^ res[index]);
+
+    const resultBinary = xorWithVal.map(bit => bit.toString(2)).join('');
+
+    return [resultBinary,res];
+}   
