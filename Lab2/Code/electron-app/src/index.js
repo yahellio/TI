c4f6265fe3230255d25a148e8455af9b6345c46d
yahellio <you@example.com>
2025-03-22 20:07:05 +0300

    Alpha version

--- Lab2/Code/electron-app/src/index.js
+++ Lab2/Code/electron-app/src/index.js
@@ -29,19 +29,20 @@ const createWindow = () => {
 
 ipcMain.handle('open-key-file', async () => {
   const result = await dialog.showOpenDialog({
-      title: 'Выберите файл с ключом',
+      title: 'Выберите файл',
       properties: ['openFile'] 
   });
 
   if (!result.canceled) {
-      const binaryData = fs.readFileSync(result.filePaths[0]);
-      const bitArray = [];
-  for (const byte of binaryData) {
-      for (let i = 7; i >= 0; i--) {
-          bitArray.push((byte >> i) & 1); // Преобразование каждого байта в биты
-      }
-  }
-      return binaryData; 
+    const binaryData = fs.readFileSync(result.filePaths[0]);
+    const bitArray = [];
+    for (const byte of binaryData) {
+        for (let i = 7; i >= 0; i--) {
+            bitArray.push((byte >> i) & 1); // Преобразование каждого байта в биты
+        }
+    }
+    const parsedPath = path.parse(result.filePaths[0]);
+    return [bitArray.join(""),`${parsedPath.name}${parsedPath.ext}`]; 
   }
 
   return null;
@@ -49,9 +50,8 @@ ipcMain.handle('open-key-file', async () => {
 
 ipcMain.handle('get-file-name', async () => {
   const result = await dialog.showOpenDialog({
-      title: 'Выберите файл с ключом',
-      filters: [{ name: 'Текстовые файлы', extensions: ['txt'] }],
-      properties: ['openFile']
+      title: 'Выберите директорию',
+      properties: ['openDirectory']
   });
 
   if (!result.canceled) {
@@ -63,7 +63,14 @@ ipcMain.handle('get-file-name', async () => {
 
 ipcMain.handle('write-in-name', async (event, path,text) => {
  
-  fs.writeFileSync(path, text);
+  const byteArr = [];
+  for(let i = 0; i<text.length; i += 8){
+    const byteStr = text.slice(i, i+8);
+    const byte = parseInt(byteStr,2);
+    byteArr.push(byte);
+  }
+
+  fs.writeFileSync(path, Buffer.from(byteArr));
 
   return null;
 });
--- Lab2/Code/react-app/src/components/TextContext.js
+++ Lab2/Code/react-app/src/components/TextContext.js
@@ -6,11 +6,12 @@ export const TextProvider = ({ children }) => {
     const [text, setText] = useState("");
     const [c, setC] = useState("");
     const [key, setKey] = useState("");
-    const [outRoad, setOutRoad] = useState("");
+    const [outRoad, setOutRoad] = useState("ecrypted.txt");
+    const [outDir, setOutDir] = useState("");
     const [genKey, setgenKey] = useState("");
 
     return (
-        <TextContext.Provider value={{ text, setText, key, setKey, outRoad, setOutRoad, c, setC, genKey, setgenKey }}>
+        <TextContext.Provider value={{ text, setText, key, setKey, outRoad, setOutRoad, c, setC, genKey, setgenKey, outDir, setOutDir }}>
             {children}
         </TextContext.Provider>
     );
